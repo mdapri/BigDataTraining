f42a6c9b8aeb9008497af72c93c3b55cad387943
mdapri <maurizio.dapri@gmail.com>
2015-03-14 11:34:43 +0100

    create metastore info

---
+++ Labs/202.Hdinsigth/CreateSqlMetastore.ps1
@@ -0,0 +1,27 @@
+﻿#reference http://azure.microsoft.com/blog/2013/02/07/windows-azure-sql-database-management-with-powershell/
+# the subscription has already been set up on this machine, otherwise follow the steps to get a certificate and set the subscription
+#Import-AzurePublishSettingsFile "C:\\Kits\\Windows Azure  MSDN - Visual Studio Premium-12-14-2014-credentials.publishsettings"
+
+#$subscription=Get-AzureSubscription
+$subscriptionName="Windows Azure  MSDN - Visual Studio Premium"
+Set-AzureSubscription -SubscriptionName $subscriptionName
+#database server already exists
+$serverName = Get-AzureSqlDatabaseServer|%{$_.ServerName}
+##credentials
+$username="mdadmin"
+$password="Av@nade001"
+
+$currentIp ="151.66.236.209"
+#add the IP to the firewall rule
+New-AzureSqlDatabaseServerFirewallRule -ServerName $serverName -RuleName “mdadmin_rule” -StartIPAddress $currentIp -EndIPAddress $currentIp
+#create credentials
+$servercredential = new-object System.Management.Automation.PSCredential($username, ($password  |    ConvertTo-SecureString -asPlainText -Force))
+
+#create a connection context
+$ctx =  New-AzureSqlDatabaseServerContext -ServerName $serverName -Credential $serverCredential
+## database creation 
+$databaseName="hdinsight_meta"
+#note that the contesxt should be used
+New-AzureSqlDatabase -ConnectionContext $ctx  -DatabaseName $databaseName -MaxSizeGB 1
+
+
--- Labs/202.Hdinsigth/ProvisioningHDinsigth.Full.ps1
+++ Labs/202.Hdinsigth/ProvisioningHDinsigth.Full.ps1
@@ -19,7 +19,6 @@ $location = "West Europe" # For example, "West US"
 $containerName="mdbigdata"
 ## Create a storage context object
 #$destContext = New-AzureStorageContext -StorageAccountName $storageAccountName  -StorageAccountKey $storageAccountKey
-
 # Create a Blob storage container
 # New-AzureStorageContainer -Name $containerName -Context $destContext
 
@@ -30,11 +29,32 @@ $containerName="mdbigdata"
 #$containerName = "mycontainer"
 $clusterName = "mdhdinsight"
 $location = "West Europe“
-$clusterNodes = 2
+$clusterNodes = 5
+## Define the PS Credentials to be used when creating the cluster:
+$hadoopUsername = "mdadmin"
+$clusterUsername = "mdadmin"
+$clusterPassword = "Av@nade001"
+
+$secpasswd = ConvertTo-SecureString $clusterPassword -AsPlainText -Force
+$clusterCreds = New-Object System.Management.Automation.PSCredential($clusterUsername, $secpasswd)
+$blobStorage = "$storageAccountName.blob.core.windows.net"
+$sqlServer = Get-AzureSqlDatabaseServer|%{$_.ServerName}
+##credentials
+$dbusername="mdadmin"
+$dbpassword="Av@nade001"
+$databaseName="hdinsight_meta"
+$databaseCreds = new-object System.Management.Automation.PSCredential($dbusername, ($dbpassword  |    ConvertTo-SecureString -asPlainText -Force))
+
 Select-AzureSubscription $subscriptionName
 $storageAccountKey = Get-AzureStorageKey $storageAccountName | %{ $_. Primary }
 
-New-AzureHDInsightCluster -ClusterSizeInNodes $clusterNodes  -Name $clusterName -Location $location -DefaultStorageAccountName "$storageAccountName.blob.core.windows.net" -DefaultStorageAccountKey $storageAccountKey -DefaultStorageContainerName $containerName 
+New-AzureHDInsightClusterConfig -ClusterSizeInNodes $clusterNodes| 
+    Set-AzureHDInsightDefaultStorage -StorageAccountName $blobStorage -StorageAccountKey $storageAccountKey -StorageContainerName $containerName |
+    Add-AzureHDInsightMetastore -SqlAzureServerName $sqlServer -DatabaseName $databaseName -Credential $databaseCreds -MetastoreType OozieMetastore |
+    New-AzureHDInsightCluster  -Credential $clusterCreds -Name $clusterName -Location $location
+
+## OLD
+#New-AzureHDInsightCluster  -Credential $clusterCreds -ClusterSizeInNodes $clusterNodes  -Name $clusterName -Location $location -DefaultStorageAccountName  $blobStorage -DefaultStorageAccountKey $storageAccountKey -DefaultStorageContainerName $containerName 
 
 Get-AzureHDInsightCluster -Name $clusterName
 
